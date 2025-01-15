452cedbe2f6200e763de9b9d1817b7a83127d13a
firmankhoirudiin <164898122+firmankhoirudiin@users.noreply.github.com>
2025-01-15 10:38:57 +0700

    Add files via upload

---
+++ pertemuan 1/pertemuan2/RumusLuasBangunDatar.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author asus
+ */
+public class RumusLuasBangunDatar {
+    
+    double phi = 3.14;
+    
+    double rumusluaslingkaran (double phi, double r){
+        double luas = phi * (r*r);
+        return luas;
+        
+    }
+    int rumusLuasPersegiPanjang(int p, int l){
+        int luas = p*l;
+        return luas;
+    }
+}
+
+    
+      
+ 
+   
